db7a976b77bfdf07b192959b1dfee9eb0c8a70b4
haemil <every.haemil@gmail.com>
2023-09-06 15:34:38 +0900

    - account domain basic CRUD

--- src/main/java/com/example/spending/domain/account/entity/Account.java
+++ src/main/java/com/example/spending/domain/account/entity/Account.java
@@ -1,14 +1,20 @@
 package com.example.spending.domain.account.entity;
 
+import com.example.spending.domain.token.entity.Symbol;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.math.BigDecimal;
 import java.time.Instant;
 
 
 @Entity
-@Table
+@Table(name = "account")
 @Getter
 public class Account {
     @Id
@@ -18,9 +24,77 @@ public class Account {
     @Column(name = "user_id")
     private Long userId;
 
-    private String tokenSymbol;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "client_id")
+    private ClientId clientId;
+
+    @Enumerated(EnumType.STRING)
+    private Symbol symbol;
 
     private BigDecimal balance;
 
-    private Instant createdAt;
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    @JsonIgnore
+    private Instant createdAt = Instant.now();
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    @JsonIgnore
+    private String createdBy;
+
+    @LastModifiedDate
+    @Column(name = "last_modified_at")
+    @JsonIgnore
+    private Instant lastModifiedAt = Instant.now();
+
+    @LastModifiedBy
+    @Column(name = "last_modified_by", length = 50)
+    @JsonIgnore
+    private String lastModifiedBy;
+
+    private void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    private void setClientId(ClientId clientId) {
+        this.clientId = clientId;
+    }
+
+    private void setSymbol(Symbol symbol) {
+        this.symbol = symbol;
+    }
+
+    private void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    private void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    private void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    private void setLastModifiedAt(Instant lastModifiedAt) {
+        this.lastModifiedAt = lastModifiedAt;
+    }
+
+    private void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public static Account create(Long userId, ClientId clientId, Symbol symbol, String createdBy) {
+        Account account = new Account();
+        account.setUserId(userId);
+        account.setClientId(clientId);
+        account.setSymbol(symbol);
+        account.setBalance(BigDecimal.ZERO);
+        account.setCreatedAt(Instant.now());
+        account.setCreatedBy(createdBy);
+        account.setLastModifiedAt(Instant.now());
+        account.setLastModifiedBy(createdBy);
+        return account;
+    }
 }
--- src/main/java/com/example/spending/domain/account/service/AccountReadService.java
+++ src/main/java/com/example/spending/domain/account/service/AccountReadService.java
@@ -1,13 +1,36 @@
 package com.example.spending.domain.account.service;
 
+import com.example.spending.domain.account.dto.AccountDto;
+import com.example.spending.domain.account.entity.Account;
+import com.example.spending.domain.account.entity.ClientId;
 import com.example.spending.domain.account.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class AccountReadService {
     private final AccountRepository accountRepository;
+
+    public AccountDto getAccount(Long id) {
+       return accountRepository.findById(id)
+               .map(this::toDto)
+               .orElseThrow(RuntimeException::new);
+    }
+
+    public List<AccountDto> getUserAccounts(Long userId, ClientId clientId) {
+       return accountRepository.findByUserIdAndClientId(userId, clientId)
+               .stream()
+               .map(this::toDto)
+               .collect(Collectors.toList());
+    }
+
+    private AccountDto toDto(Account account) {
+        return new AccountDto(account.getId(), account.getUserId(), account.getClientId(), account.getSymbol(), account.getBalance());
+    }
 }
