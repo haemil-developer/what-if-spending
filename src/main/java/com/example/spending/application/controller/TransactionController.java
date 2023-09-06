6fb4db1b68b1ee6d4b67841a7909e353b582729b
haemil <every.haemil@gmail.com>
2023-09-06 16:44:47 +0900

    transaction domain basic CRUD

---
+++ src/main/java/com/example/spending/application/controller/TransactionController.java
@@ -0,0 +1,36 @@
+package com.example.spending.application.controller;
+
+import com.example.spending.domain.account.entity.ClientId;
+import com.example.spending.domain.transaction.dto.CreateTransactionCommand;
+import com.example.spending.domain.transaction.dto.TransactionDto;
+import com.example.spending.domain.transaction.entity.Type;
+import com.example.spending.domain.transaction.service.TransactionReadService;
+import com.example.spending.domain.transaction.service.TransactionWriteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/transactions")
+@RequiredArgsConstructor
+public class TransactionController {
+    private final TransactionReadService transactionReadService;
+    private final TransactionWriteService transactionWriteService;
+
+    @PostMapping("/pay")
+    public void pay(@RequestBody CreateTransactionCommand command) {
+        transactionWriteService.createTransaction(Type.WITHDRAWAL, command);
+    }
+
+    @PostMapping("/reward")
+    public void reward(@RequestBody CreateTransactionCommand command) {
+        transactionWriteService.createTransaction(Type.DEPOSIT, command);
+    }
+
+    @GetMapping("/users/{userId}/clients/{clientId}")
+    public List<TransactionDto> getUserTransactions(@PathVariable("userId") Long userId,
+                                                    @PathVariable("clientId") ClientId clientId) {
+        return transactionReadService.getUserTransactions(userId,clientId);
+    }
+}
--- src/main/java/com/example/spending/domain/transaction/service/TransactionWriteService.java
+++ src/main/java/com/example/spending/domain/transaction/service/TransactionWriteService.java
@@ -1,11 +1,29 @@
 package com.example.spending.domain.transaction.service;
 
+import com.example.spending.domain.transaction.dto.CreateTransactionCommand;
+import com.example.spending.domain.transaction.entity.Transaction;
+import com.example.spending.domain.transaction.entity.Type;
 import com.example.spending.domain.transaction.repository.TransactionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class TransactionWriteService {
     private final TransactionRepository transactionRepository;
+
+    public void createTransaction(Type type, CreateTransactionCommand command) {
+        Transaction transaction = Transaction.create(
+                command.userId(),
+                command.clientId(),
+                type,
+                command.title(),
+                command.description(),
+                command.symbol(),
+                command.amount(),
+                "createdBy");
+        transactionRepository.save(transaction);
+    }
 }
